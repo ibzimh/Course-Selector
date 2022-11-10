039a5413f4b80573094a10002f2f1fbd1e0a60a7
ibzimh <115051423+ibzimh@users.noreply.github.com>
2022-11-10 05:53:29 -0500

    Update txtParserToJson.py

--- txtParserToJson.py
+++ txtParserToJson.py
@@ -1,27 +1,9 @@
 import json
-
-def reduce(arr, func, init):
-  acc = init
-  for elem in arr:
-    acc = func(acc, elem)
-  return acc
-
-def reduceind(arr, func, init):
-  acc = init
-  for ind, elem in enumerate(arr):
-    acc = func(acc, elem, ind)
-  return acc
   
 def map(arr, func):
   ret = []
   for elem in arr:
     ret.append(func(elem))
-  return ret
-
-def mapind(arr, func):
-  ret = []
-  for ind, elem in enumerate(arr):
-    ret.append(func(elem, ind))
   return ret
 
 def filter(arr, func):
